7a9e873efe1c9b5c4bcfe8a64f6da0f524bb9df6
santoshkalukunda <santoshkalukunda@gmail.com>
2023-03-15 13:30:46 +0545

    dashboard updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+@php
+    $title = "Dashboard";
+@endphp
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ $title = 'Dashboard' }}</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                        {{ __('You are logged in!') }}
-                    </div>
+                <div class="text-center font-weight-bold py-2" style="margin-top: 5%">
+                    <img src="{{ asset('img/logo.png') }}" alt="{{ env('APP_NAME') }}" class="my-1" height="100">
+                    <h6>नेपाल सरकार</h6>
+                    <h3>सुदूरपश्चिम प्रदेश</h3>
+                    <h4>{{Auth::user()->office->name}}</h4>
+                    <h5>आयोजना व्यवस्थापन सूचना प्रणाली</h5>
                 </div>
             </div>
         </div>
