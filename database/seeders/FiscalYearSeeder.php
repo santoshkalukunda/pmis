cff3612a67a62621f1ca40ee882362d176321875
santoshkalukunda <santoshkalukunda@gmail.com>
2023-03-15 14:26:49 +0545

    fiscal year seeder adding

--- database/seeders/FiscalYearSeeder.php
+++ database/seeders/FiscalYearSeeder.php
@@ -20,16 +20,40 @@ class FiscalYearSeeder extends Seeder
                 'fiscal_year' => '2079/80',
             ],
             [
-                'fiscal_year' => '2079/78',
+                'fiscal_year' => '2078/79',
             ],
             [
-                'fiscal_year' => '2078/77',
+                'fiscal_year' => '2077/78',
             ],
             [
-                'fiscal_year' => '2077/76',
+                'fiscal_year' => '2076/77',
             ],
             [
-                'fiscal_year' => '2076/75',
+                'fiscal_year' => '2075/76',
+            ],
+            [
+                'fiscal_year' => '2074/75',
+            ],
+            [
+                'fiscal_year' => '2073/74',
+            ],
+            [
+                'fiscal_year' => '2072/73',
+            ],
+            [
+                'fiscal_year' => '2071/72',
+            ],
+            [
+                'fiscal_year' => '2070/71',
+            ],
+            [
+                'fiscal_year' => '2069/70',
+            ],
+            [
+                'fiscal_year' => '2068/69',
+            ],
+            [
+                'fiscal_year' => '2067/68',
             ],
         ];
         foreach ($fiscalYears as $fiscalYear) {
